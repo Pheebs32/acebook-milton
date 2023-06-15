7a647f9869e916e3ed993a4bf0220b9669ba89f2
Saamiya96 <saamiyayousuf@hotmail.co.uk>
2023-06-15 16:39:10 +0100

    will revert changes back

--- models/user.js
+++ models/user.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {
+    type: String, 
+    required: true
+    // required: () => {
+    //   return this.username != "";
+    // },
+  },
+  email: { 
+    type: String, 
+    required: true,
+    // required: () => {
+    //   return this.email != "";
+    // },
+    unique: true,
+    lowercase: true
+  },
+  password: { 
+    type: String, 
+    required: true 
+    // required: () => {
+    //   return this.password != ""
+    // },
+  },
   posts: [
     {
       content: String,
