d348e398a7bb12d7bc91a61a5e6acd5a58f06027
Saamiya96 <saamiyayousuf@hotmail.co.uk>
2023-06-17 20:05:33 +0100

    minor changes

--- models/user.js
+++ models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {
+    type: String, 
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String, 
+    required: true 
+  },
   posts: [
     {
       content: String,
