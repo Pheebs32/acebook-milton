f412d73baeb4e8f4457dd4dc29e46441f8bbdfe1
Barlowi2023 <ian.m.barlow1@googlemail.com>
2023-06-21 10:51:17 +0100

    resolved conflicts

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,7 +8,6 @@ const PostsController = {
       }
       // const dateFormatted = `${posts[0].date.getHours()}:${posts[0].date.getMinutes()}, ${posts[0].date.toDateString()}`;
       // console.log(dateFormatted);
-
       
       const sortedPosts = posts.flat().sort((a, b) => b.date - a.date);
       // sortedPosts[0].date = dateFormatted;
@@ -21,21 +20,74 @@ const PostsController = {
     res.render("posts/new", {user: req.session.user, isAuthenticated: true});
   },
 
+  Create: (req, res) => {
+    const { message, likes } = req.body;
+    const username = req.session.user.username;
+    const post = new Post({ username, message, likes });
+    
+    console.log(`message ${post.message}`)
 
-  AddLike: async (req, res) => {
-    // get the post_id for the 
+    if (post.message == '' ) {
+      res.render('posts/new', {error: "The post cannot be left blank!"});
+    } else {
+      post.save((err) => {
+        if (err) {
+          throw err;
+        }
+
+        res.status(201).redirect("/posts");
+      });
+    }
+  },
+
+  Edit: async (req, res) => { 
     const post_id = req.params.id;
     console.log(`Post_id ${post_id}`);
-
     const user_id = req.session.user._id;
     console.log(`user_id ${user_id}`);
 
     const post = await Post.findOne({ _id: post_id });
     console.log(`post ${post}`);
 
+    res.render("posts/edit", { post: post , user: req.session.user, isAuthenticated: true});
+
+  },
+
+  Update: async (req, res) => { 
+    const post_id = req.params.id;
+    console.log(`Post_id ${post_id}`);
+    const user_id = req.session.user._id;
+    console.log(`user_id ${user_id}`);
+
+    const post = await Post.findOne({ _id: post_id });
+    console.log(`post ${post}`);
+    post.message = req.body.content 
+    await post.save()
+    res.status(201).redirect(`/users/${req.session.user.username}`); 
+
+  },
+
+  Delete: async (req, res) => { 
+    const post_id = req.params.id;
+    console.log(`Post_id ${post_id}`);
+    const user_id = req.session.user._id;
+    console.log(`user_id ${user_id}`);
+
+    const post = await Post.findOne({ _id: post_id })
+    await post.deleteOne({ _id: post_id});
+    res.status(201).redirect(`/users/${req.session.user.username}`); 
+  },
+
+  AddLike: async (req, res) => {
+    // get the post_id for the 
+    const post_id = req.params.id;
+    console.log(`Post_id ${post_id}`);
+    const user_id = req.session.user._id;
+    console.log(`user_id ${user_id}`);
+    const post = await Post.findOne({ _id: post_id });
+    console.log(`post ${post}`);
     const likes = post.like;
     console.log(`likes ${likes}`);
-
     const liked = post.like.map((like) => like.likeAuthor).includes(user_id);
     console.log(`liked ${liked}`);
     
@@ -54,28 +106,6 @@ const PostsController = {
     // () => {res.status(201).redirect("/posts");
   },
 
-  Create: (req, res) => {
-  const { message, likes } = req.body;
-  const username = req.session.user.username;
-  // const now = new Date();
-  // const options = { 
-  //   weekday: 'short', 
-  //   month: 'short', 
-  //   day: 'numeric', 
-  //   year: 'numeric', 
-  //   hour: 'numeric', 
-  //   minute: 'numeric' 
-  // };
-  // const currentDate = now.toLocaleString('en-US', options);
-  const post = new Post({ username, message, likes });
-    post.save((err) => {
-      if (err) {
-        throw err;
-      }
-
-      res.status(201).redirect("/posts");
-    });
-  },
 };
 
 
--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,9 @@ router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.get("/new", PostsController.New);
 router.post("/:id/likes", PostsController.AddLike);
-// router.post("/like", PostsController.AddLike);
+router.get("/:id/edit", PostsController.Edit);  
+router.post("/:id", PostsController.Update);
+router.post("/:id/delete", PostsController.Delete);
 
 
 module.exports = router;
