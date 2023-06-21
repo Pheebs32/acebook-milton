9b5ce610468bbe91fbc425310af62fbea5f12ad4
Phoebe Swaine <peswaine@gmail.com>
2023-06-21 16:31:10 +0100

    Updated cancel_button test

--- cypress/integration/sign_up/signup_cancel_button.js
+++ cypress/integration/sign_up/signup_cancel_button.js
@@ -8,16 +8,11 @@ describe("User Sign Up - Cancel Button Functionality", () => {
     cy.get("#username").type("Tester");
     cy.get("#email").type("Test@test.com");
     cy.get("#password").type("Tester1!");
-    cy.get("#submit-signup-button").click();
-
-    // visit the log in page, fill fields and click cancel
-    cy.visit("/sessions/login");
-    cy.get("#email").type("test@test.com");
-    cy.get("#password").type("oooooooo");
     cy.get("#cancel").click();
-    
-    // check that email and password fields are cleared
 
+    // check that username, email and password fields are cleared
+
+    cy.get("#username").should('have.value', '');
     cy.get("#email").should('have.value', '');
     cy.get("#password").should('have.value', '');
     });
